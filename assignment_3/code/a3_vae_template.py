7a71575d691d3ea8d3047cd9b85642e16c73bcd1
Daniel <dfdazac@gmail.com>
2019-05-10 22:09:33 +0200

    Train VAE

--- assignment_3/code/a3_vae_template.py
+++ assignment_3/code/a3_vae_template.py
@@ -16,12 +16,15 @@ ex = Experiment()
 uri = os.environ.get('MLAB_URI')
 database = os.environ.get('MLAB_DB')
 if all([uri, database]):
+    print(uri)
+    print(database)
     ex.observers.append(MongoObserver.create(uri, database))
 
 IMG_WIDTH = 28
 IMG_HEIGHT = 28
 IMG_PIXELS = IMG_WIDTH * IMG_HEIGHT
 
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 class Encoder(nn.Module):
     def __init__(self, hidden_dim=500, z_dim=20):
@@ -97,7 +100,7 @@ class VAE(nn.Module):
         (from bernoulli) and the means for these bernoullis (as these are
         used to plot the data manifold).
         """
-        z = torch.randn((n_samples, self.z_dim))
+        z = torch.randn((n_samples, self.z_dim)).to(device)
         im_means = torch.sigmoid(self.decoder(z))
         sampled_ims = torch.bernoulli(im_means)
 
@@ -114,6 +117,7 @@ def epoch_iter(model, data, optimizer):
     average_epoch_elbo = 0
 
     for imgs in data:
+        imgs = imgs.to(device)
         rec_loss, kl = model(imgs.view(-1, IMG_PIXELS))
         avg_elbo = rec_loss + kl
         average_epoch_elbo += avg_elbo.item()
@@ -155,25 +159,30 @@ def save_elbo_plot(train_curve, val_curve, filename):
     plt.savefig(filename)
 
 
-def save_samples(model, fname):
+@ex.capture
+def save_samples(model, fname, _run):
     samples, _ = model.sample(n_samples=16)
-    samples = samples.cpu().detach()
+    samples = samples.detach().cpu()
     grid = make_grid(samples.reshape(-1, 1, IMG_WIDTH, IMG_HEIGHT),
                      nrow=4)
     plt.cla()
     plt.imshow(grid.permute(1, 2, 0).numpy())
     plt.axis('off')
-    plt.savefig(fname)
+    img_path = os.path.join(os.path.dirname(__file__), 'saved', fname)
+    plt.savefig(img_path)
+    _run.add_artifact(img_path, fname)
+    os.remove(img_path)
 
 
 @ex.main
 def main(epochs, zdim, _run):
     data = bmnist()[:2]  # ignore test split
-    model = VAE(z_dim=zdim)
+    model = VAE(z_dim=zdim).to(device)
     optimizer = torch.optim.Adam(model.parameters())
 
     for epoch in range(1, epochs + 1):
-        if 100 * (epoch//epochs) in [0, 50, 100]:
+        # Save samples at beginning, 50% and 100% of training
+        if int(100 * epoch/epochs) in [int(100/epochs), 50, 100]:
             fname = 'samples_{:d}.png'.format(epoch)
             save_samples(model, fname)
 
